c4bee29f9d29e36e2aff4e9f28f4a1a160cb5ad9
Lena Hou <lenahou2013@gmail.com>
2021-01-25 22:01:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+<<<<<<< HEAD
 if board[index] == " " || board[index] == "" || board[index] == nil
+=======
+if board[index] == " " or "" or nil
+>>>>>>> c8c37741a5bfc89b16de705b3bad536a904e729a
    return false
 else board[index] == "X" || board[index] == "O"
   return true
